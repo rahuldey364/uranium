26c43c6fe771a182bf01297d627ff733950e7fc6
rahuldey364 <rahuldey364@gmail.com>
2022-04-08 09:05:58 +0530

    my new branch

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,76 @@
 const express = require('express');
-const logger = require('./logger')
+
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/movies', function (req, res) {
+    const movies = ["rand de basnasti", "the shining", "lord of the rings", "bartman begins"]
+    res.send(movies)
 });
+
+router.get('/movies/:indexNumber', function(req,res){
+    const movies = ["rand de basnasti", "the shining", "lord of the rings", "bartman begins"]
+    const indexNumber=req.params.indexNumber;
+    const result = movies[(indexNumber-1)]
+    if (indexNumber>movies.length){
+        res.send("use a valid index")
+    }else{
+    res.send(result)
+    }
+});
+
+
+router.get('/films', function (req, res) {
+    const films = [ {
+        id: 1,
+        name: "The Shining"
+       }, 
+       {
+        id: 2,
+        name: "Incendies"
+       }, 
+       {
+        id: 3,
+        name: "Rang de Basanti"
+       }, 
+       {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+       
+    res.send(films)
+});
+
+
+router.get('/films/:filmId', function (req, res) {
+    const filmId=req.params.filmId;
+    const films = [ {
+        id: 1,
+        name: "The Shining"
+       }, 
+       {
+        id: 2,
+        name: "Incendies"
+       }, 
+       {
+        id: 3,
+        name: "Rang de Basanti"
+       }, 
+       {
+        id: 4,
+        name: "Finding Nemo"
+    }]
+    for(i=0;i<films.length;i++){
+        if (filmId==films[i].id){
+            res.send(films[i])
+            return
+        }
+    }
+    res.send("No movie exists with this id")   
+});
+
+
+
 
 
 
