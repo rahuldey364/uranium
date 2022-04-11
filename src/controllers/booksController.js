ab05f275527bfd2e391e8de92f2b8e437cc0e9b2
rahuldey364 <rahuldey364@gmail.com>
2022-04-12 00:02:41 +0530

    first mongo

---
+++ src/controllers/booksController.js
@@ -0,0 +1,15 @@
+const booksModel= require("../models/booksModel")
+
+const createBook= async function (req, res) {
+    let data= req.body
+    let savedData= await booksModel.create(data)
+    res.send({msg: savedData})
+}
+
+const getBooks= async function (req, res) {
+    let allBooks= await booksModel.find()
+    res.send({msg: allBooks})
+}
+
+module.exports.createBook= createBook
+module.exports.getBooks  = getBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const booksController= require("../controllers/booksController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/createBook", booksController.createBook  )
+
+router.get("/getBooks", booksController.getBooks)
 
 module.exports = router;
