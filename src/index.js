28d624ade2b408e6d1934092c7949d3bbc8499b7
rahuldey364 <rahuldey364@gmail.com>
2022-04-20 01:43:16 +0530

    new branch added

--- src/index.js
+++ src/index.js
@@ -2,24 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+var dateTime = require('node-datetime');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://FunctionUp-cohort-1:TENBQZypGNobyoq3@cluster0.7jbgo.mongodb.net/Book-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        var ip = req.socket.remoteAddress;
+        var dt = dateTime.create();
+        var formatted = dt.format('Y-m-d H:M:S');
+        console.log(formatted + " , " + ip + " , " + req.originalUrl);
         next();
-  }
-  );
+    }
+);
 
 app.use('/', route);
 
