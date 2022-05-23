71fa2ce646407dcd80ecabfe9322fbbb5f2dd08b
rahuldey364 <rahuldey364@gmail.com>
2022-05-23 15:49:48 +0530

    final

--- src/models/publisherModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const ObjectId = mongoose.Schema.Types.ObjectId
-
-const newPublisherSchema = new mongoose.Schema({
-    name: String,
-    headQuarter:String
-},{timestamps:true})
-
-module.exports= mongoose.model('newPublisher' , newPublisherSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,29 +1,60 @@
 const express = require('express');
 const router = express.Router();
-
-const authorController= require("../controllers/authorController")
-const bookController= require("../controllers/bookController")
-const publisherController = require("../controllers/publisherController")
+const aws= require("aws-sdk")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createAuthor", authorController.createAuthor  )
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
 
-router.get("/getAuthorsData", authorController.getAuthorsData)
+let uploadFile= async ( file) =>{
+   return new Promise( function(resolve, reject) {
+    let s3= new aws.S3({apiVersion: '2006-03-01'}); 
 
-router.post("/createNewPublisher", publisherController.createNewPublisher  )
+    var uploadParams= {
+        ACL: "public-read",
+        Bucket: "classroom-training-bucket", 
+        Key: "Rahul/" + file.originalname, 
+        Body: file.buffer
+    }
 
-router.get("/getPublisherData", publisherController.getPublisherData)
 
-router.post("/createBook", bookController.createBook  )
+    s3.upload( uploadParams, function (err, data ){
+        if(err) {
+            return reject({"error": err})
+        }
+        // console.log(data)
+        console.log("file uploaded succesfully")
+        return resolve(data.Location)
+    })
 
-router.get("/getBooksData", bookController.getBooksData)
 
-router.put("/newData", bookController.newData)
+   })
+}
 
-router.put("/saleIncrease", bookController.saleIncrease)
+router.post("/write-file-aws", async function(req, res){
 
+    try{
+        let files= req.files
+        if(files && files.length>0){
+            
+            let uploadedFileURL= await uploadFile( files[0] )
+            res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
+        }
+        else{
+            res.status(400).send({ msg: "No file found" })
+        }
+        
+    }
+    catch(err){
+        res.status(500).send({msg: err})
+    }
+    
+})
 
 module.exports = router;
